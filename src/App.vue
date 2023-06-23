a9630da21211b764894e5bc7d5d49930195f278c
Niu Roso <83788547+Junior0792@users.noreply.github.com>
2023-06-23 16:52:09 -0300

    Novo

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,81 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <button @click="openPopup('caça-níquel')">Abrir Pop-up Caça-níquel</button>
+    <button @click="openPopup('vídeo')">Abrir Pop-up de Vídeo</button>
+
+    <div v-if="showPopup">
+      <div class="popup">
+        <h2>{{ popupData.title }}</h2>
+        <h3>{{ popupData.subtitle }}</h3>
+
+        <div v-if="popupType === 'caça-níquel'">
+        <img src="@/assets/caca-niquel.png" width="300" height="200" alt="Descrição da imagem">
+
+          <!-- Conteúdo do jogo caça-níquel -->
+          <h2>{{ popupData.content}}</h2>
+        </div>
+
+        <div v-if="popupType === 'vídeo'">
+          <!-- Reprodutor de vídeo -->
+          <video :src="popupData.content" controls></video>
+        </div>
+
+        <form>
+          <div v-for="(field, index) in popupData.formFields" :key="index">
+            <label :for="field">{{ field }}</label>
+            <input :type="field" :id="field" />
+          </div>
+
+          <div v-if="popupData.consentCheckbox">
+            <label for="consentCheckbox">
+              <input type="checkbox" id="consentCheckbox" />
+              Consentimento para coleta de dados
+            </label>
+          </div>
+
+          <button>Enviar</button>
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import popupData from "./popupData.json";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data() {
+    return {
+      showPopup: false,
+      popupType: "",
+      popupData: {}
+    };
+  },
+  methods: {
+    openPopup(type) {
+      this.showPopup = true;
+      this.popupType = type;
+      this.popupData = popupData[type];
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.popup {
+  position:fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 400px;
+  padding: 20px;
+  background-color: #ffff;
+  border: 2px solid #000000;
 }
+
+
+
+
 </style>
+
